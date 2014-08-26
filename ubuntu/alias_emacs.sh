d409a76e3648c1b3595f362a4a6b6c415d05dcc0
Mike Seidle <mike@mikeseidle.com>
2014-08-26 10:18:13 -0400

    Easy way to make sure you get the emacs you want on Ubuntu

---
+++ ubuntu/alias_emacs.sh
@@ -0,0 +1,8 @@
+# Add this to your .bashrc or .bash_profile to 
+# make it easier to start up the emacs you want.
+# $ xmacs # gets graphical version
+# $ emacs # starts console version
+
+alias xmacs='emacs24-x'
+alias emacs='emacs -nw'
+
